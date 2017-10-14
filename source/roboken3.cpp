7fa41185000f3f2705ec826fc1fb94c464c6449c
ringo156 <blackapple262@gmail.com>
2017-10-14 10:40:19 +0900

    add roboken3.cpp

---
+++ source/roboken3.cpp
@@ -0,0 +1,260 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ode/ode.h>
+#include <drawstuff/drawstuff.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+
+
+#ifdef _MSC_VER
+#pragma warning(disable:4244 4305)  // for VC++, no precision loss complaints
+#endif
+
+#ifdef dDOUBLE
+#define dsDrawBox      dsDrawBoxD
+#define dsDrawSphere   dsDrawSphereD
+#define dsDrawCapsule  dsDrawCapsuleD
+#define dsDrawCylinder dsDrawCylinderD
+#define ID 0x55
+#define SERIAL_PORT "/dev/ttyUSB0"
+#define BAUD_RATE B19200
+#endif
+#define NUM 4                          // リンク数
+
+dWorldID      world;                   // 動力学計算用のワールド
+dSpaceID      space;                   // 衝突検出用のスペース
+dGeomID       ground;                  // 地面のジオメトリID番号
+dJointGroupID contactgroup;            // 接触点グループ
+dJointID      joint[NUM];              // ジョイントのID番号
+dsFunctions   fn;                      // ドロースタッフの描画関数
+
+int fd;
+
+typedef struct {
+  dBodyID body;                        // ボディのID番号
+  dGeomID geom;                        // ジオメトリのID番号
+} MyObject;                            // MyObject構造体
+
+union UFloatLong {
+  long	lLong;
+  float	fFloat;
+};
+
+MyObject rlink[NUM], base;                   // リンク
+dReal  THETA[NUM] = {0.0};             // 関節の目標角度[rad]
+static const dReal BASE1[3] = {0.041, 0.031, 0.3};
+
+void read_buf(void){
+    unsigned char buf[255];
+    int len, i;
+
+    len = read(fd, buf, sizeof(buf));
+    if (0 < len) {
+        for(i = 0; i < len; i++) {
+            printf("%02X", buf[i]);
+        }
+        printf("\n");
+    }
+
+}
+
+int serial_init(int baudRate){    
+    unsigned char msg[] = "serial port open...\n";
+    struct termios tio;
+    fd = open(SERIAL_PORT, O_RDWR);     // デバイスをオープンする
+    if (fd < 0) {
+        printf("open error\n");
+        return -1;
+    }
+    tio.c_cflag += CREAD;               // 受信有効
+    tio.c_cflag += CLOCAL;              // ローカルライン（モデム制御なし）
+    tio.c_cflag += CS8;                 // データビット:8bit
+    tio.c_cflag += 0;                   // ストップビット:1bit
+    tio.c_cflag += 0;                   // パリティ:None
+
+    cfsetispeed( &tio, baudRate );
+    cfsetospeed( &tio, baudRate );
+
+    cfmakeraw(&tio);                    // RAWモード
+
+    tcsetattr( fd, TCSANOW, &tio );     // デバイスに設定を行う
+
+    ioctl(fd, TCSETS, &tio);            // ポートの設定を有効にする
+
+    return 0;
+
+}
+
+void tx_angle(float angle, int link)
+{
+  union UFloatLong func;
+  unsigned char tx[6];
+  func.fFloat = angle;
+  tx[0] = ID;
+  tx[1] = link;
+  tx[2] = func.lLong >> 24;
+  tx[3] = func.lLong >> 16;
+  tx[4] = func.lLong >> 8;
+  tx[5] = func.lLong;
+  write(fd, tx, sizeof(tx));
+
+}
+
+/*** ロボットアームの生成 ***/
+void  makeArm()
+{
+  dMass mass;                                    // 質量パラメータ
+  dReal x[NUM]      = {0.00, 0.01, 0.00, 0.00};  // 重心 x
+  dReal y[NUM]      = {0.00, 0.00, 0.00, 0.00};  // 重心 y
+  dReal z[NUM]      = {0.35, 0.31, 0.3365, 0.377};  // 重心 z
+  dReal length[NUM] = {0.10, 0.021, 0.031, 0.05};  // 長さ
+  dReal weight[NUM] = {9.00, 2.00, 2.00, 2.00};  // 質量
+  dReal r[NUM]      = {0.20, 0.04, 0.04, 0.04};  // 半径
+  dReal c_x[NUM]    = {0.00, 0.00, 0.00, 0.00};  // 関節中心点 x
+  dReal c_y[NUM]    = {0.00, 0.00, 0.00, 0.00};  // 関節中心点 y
+  dReal c_z[NUM]    = {0.30, 0.3, 0.311, 0.342};  // 関節中心点 z
+  dReal axis_x[NUM] = {0, 0, 0, 0};              // 関節回転軸 x
+  dReal axis_y[NUM] = {0, 0, 1, 1};              // 関節回転軸 y
+  dReal axis_z[NUM] = {1, 1, 0, 0};              // 関節回転軸 z
+
+  // リンクの生成
+  for (int i = 1; i < NUM; i++) {
+    rlink[i].body = dBodyCreate(world);
+    dBodySetPosition(rlink[i].body, x[i], y[i], z[i]);
+    dMassSetZero(&mass);
+    //dMassSetCapsuleTotal(&mass,weight[i],3,r[i],length[i]);
+    dMassSetBoxTotal(&mass, weight[i], 2*r[i], 2*r[i], length[i]);
+    dBodySetMass(rlink[i].body, &mass);
+    //rlink[i].geom = dCreateCapsule(space,r[i],length[i]);
+    rlink[i].geom = dCreateBox(space, 2*r[i], 2*r[i], length[i]);
+    dGeomSetBody(rlink[i].geom,rlink[i].body);
+  }
+  base.body = dBodyCreate(world);
+  dBodySetPosition(base.body, 0.01, 0.00, 0.15);
+  dMassSetZero(&mass);
+  dMassSetBoxTotal(&mass, 9.00, 0.2, 0.3, 0.3);
+  dBodySetMass(base.body, &mass);
+  base.geom = dCreateBox(space, 0.2, 0.3, 0.4);
+  dGeomSetBody(base.geom, base.body);
+
+  // ジョイントの生成とリンクへの取り付け
+  //joint[0] = dJointCreateFixed(world, 0);  // 固定ジョイント
+  //dJointAttach(joint[0], rlink[0].body, 0);
+  //dJointSetFixed(joint[0]);
+  
+  joint[0] = dJointCreateFixed(world, 0);  // 固定ジョイント
+  dJointAttach(joint[0], base.body, 0);
+  dJointSetFixed(joint[0]);
+
+  for (int j = 1; j < NUM; j++) {
+    joint[j] = dJointCreateHinge(world, 0); // ヒンジジョイント
+    dJointAttach(joint[j], rlink[j].body, rlink[j-1].body);
+    dJointSetHingeAnchor(joint[j], c_x[j], c_y[j], c_z[j]);
+    dJointSetHingeAxis(joint[j], axis_x[j], axis_y[j],axis_z[j]);
+  }
+}
+
+/*** ロボットアームの描画 ***/
+void drawArm()
+{
+    dReal r,length;
+    dReal *p[4];
+    dReal length1[NUM][3] = {{0.021, 0.031, 0.010},
+                             {0.041, 0.035, 0.021},
+                             {0.018, 0.045, 0.031},
+                             {0.028, 0.031, 0.050}};  // 長さ
+
+    for(int i=0;i<NUM;i++){
+        p[i] = length1[i];
+    }
+   dsDrawBox(dGeomGetPosition(base.geom), dGeomGetRotation(base.geom), BASE1);
+   
+   for (int i = 1; i < NUM; i++ ) {       // カプセルの描画
+     dsDrawBox(dGeomGetPosition(rlink[i].geom), dGeomGetRotation(rlink[i].geom), &length1[i][0]);
+   }
+   
+}
+
+/*** P制御 ***/
+void Pcontrol()
+{
+  dReal k =  10.0, fMax = 100.0;                   // 比例ゲイン，最大トルク
+  static float angle_now[NUM], angle_old[NUM];
+
+  for (int j = 1; j < NUM; j++) {
+    dReal tmp = dJointGetHingeAngle(joint[j]);     // 関節角の取得
+    angle_now[j] = tmp;
+    if(angle_old[j] != angle_now[j]){
+      printf("joint%d : %f\n", j, angle_now[j]);
+      tx_angle(angle_now[j], j);
+    }
+    dReal z = THETA[j] - tmp;                      // 残差
+    dJointSetHingeParam(joint[j],dParamVel, k*z);  // 角速度の設定
+    dJointSetHingeParam(joint[j],dParamFMax,fMax); // トルクの設定
+    angle_old[j] = angle_now[j];
+  }
+}
+
+/*** 視点と視線の設定 ***/
+void start()
+{
+  float xyz[3] = {    3.0f, 1.3f, 0.8f};          // 視点[m]
+  float hpr[3] = { -160.0f, 4.5f, 0.0f};          // 視線[°]
+  dsSetViewpoint(xyz, hpr);                       // 視点と視線の設定
+}
+
+/*** キー入力関数 ***/
+void command(int cmd)
+{
+  switch (cmd) {
+    case 'j': THETA[1] += M_PI/180; break;     // jキー
+    case 'f': THETA[1] -= M_PI/180; break;     // fキー
+    case 'k': THETA[2] += M_PI/180; break;     // kキー
+    case 'd': THETA[2] -= M_PI/180; break;     // dキー
+    case 'l': THETA[3] += M_PI/180; break;     // lキー
+    case 's': THETA[3] -= M_PI/180; break;     // sキー
+  }
+}
+
+/*** シミュレーションループ ***/
+void simLoop(int pause)
+{
+  Pcontrol();                                  // P制御
+  dWorldStep(world, 0.01);                     // 動力学計算
+  drawArm();                                   // ロボットの描画
+
+  
+}
+
+/*** ドロースタッフの設定 ***/
+void setDrawStuff()
+{
+  fn.version = DS_VERSION;                     // バージョン番号
+  fn.start   = &start;                         // start関数
+  fn.step    = &simLoop;                       // simLoop関数
+  fn.command = &command;                       // command関数
+  fn.path_to_textures = "drawstuff/textures";
+}
+
+int main(int argc, char **argv)
+{
+  serial_init(BAUD_RATE);
+  dInitODE();                                     // ODEの初期化
+  setDrawStuff();
+  world        = dWorldCreate();                  // ワールドの生成
+  space        = dHashSpaceCreate(0);             // スペースの生成
+  contactgroup = dJointGroupCreate(0);            // 接触グループの生成
+  ground       = dCreatePlane(space, 0, 0, 1, 0); // 地面の生成
+  dWorldSetGravity(world, 0, 0, -9.8);            // 重力の設定
+  makeArm();                                      // アームの生成
+  dsSimulationLoop(argc, argv, 640, 480, &fn);    // シミュレーションループ
+  dSpaceDestroy(space);                           // スペースの破壊
+  dWorldDestroy(world);                           // ワールドの破壊
+  dCloseODE();                                    // ODEの終了
+  return 0;
+}
